e64c9fd46cd11776370f3587c048354fb6008bee
cqh <admin@zgswl.com>
2020-06-25 04:02:02 +0800

    新建登录分支新建登录组件配置路由

---
+++ src/components/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        msg: 'Welcome to your login'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
