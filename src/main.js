4f076c74ae693554e7330fa18bc4f8594c56e135
cqh <admin@zgswl.com>
2020-06-25 11:33:05 +0800

    引入提示框

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
+import ElementUI from 'element-ui'
+import MyServerHttp from '@/pluginx/http.js'
+
+import axios from 'axios'
+
+// 不要忘记引入css文件
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/reset.css'
 
 import router from './router'
+Vue.prototype.$http = axios
 
+// 适用Vue插件
 Vue.use(ElementUI)
+Vue.use(MyServerHttp)
 
 Vue.config.productionTip = false
 
