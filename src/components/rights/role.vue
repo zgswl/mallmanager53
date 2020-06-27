6691b81394967776f7efccbad798bb7c0762140c
cqh <admin@zgswl.com>
2020-06-27 16:19:42 +0800

    '权限的颜色+关闭+图标'

--- src/components/rights/role.vue
+++ src/components/rights/role.vue
@@ -47,22 +47,24 @@
 
     <!-- 表格 -->
     <el-table :data="rolelist" style="width: 100%">
-      <el-table-column type="expand" width="50">
+      <el-table-column type="expand" width="150">
         <template slot-scope="scope">
           <el-row v-for="(item1,i) in scope.row.children" :key="i">
             <el-col :span="4">
-              <el-tag>{{item1.authName}}</el-tag>
+              <el-tag closable type="">{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
             </el-col>
             <el-col :span="20">
               <el-row v-for="(item2,i) in item1.children" :key="i">
-                <el-col :span="4">
-                  <el-tag>{{item2.authName}}</el-tag>
-                </el-col>
-                <el-col :span="20">
 
-                  <el-tag v-for="(item3,i) in item2.children" :key="i">
-                    {{item3.authName}}
-                  </el-tag>
+                <el-col :span="4">
+                  <el-tag closable type="success">{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+
+                <el-col :span="20">
+                  <el-tag closable type="warning" v-for="(item3,i) in item2.children" :key="i">
+                    {{item3.authName}}</el-tag>
                 </el-col>
               </el-row>
             </el-col>
@@ -70,7 +72,7 @@
         </template>
       </el-table-column>
 
-      <el-table-column type="index" label="#" width="80"> </el-table-column>
+      <el-table-column type="index" label="#" width="150"> </el-table-column>
 
       <!--
         roleName: "主管"
