db8576aca9499ad6a320a0632403ee34d88bad67
cqh <admin@zgswl.com>
2020-06-27 16:27:50 +0800

    '无权限时的提示'

--- src/components/rights/role.vue
+++ src/components/rights/role.vue
@@ -69,6 +69,9 @@
               </el-row>
             </el-col>
           </el-row>
+
+          <!-- 无权限的提示 -->
+          <span v-if="scope.row.children.length===0">未分配权限</span>
         </template>
       </el-table-column>
 
